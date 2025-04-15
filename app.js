9ba47d529b6e8d515b7a75fccdad387dbf536e88
Mark Sazonov <mark.saznov@tktk.ee>
2025-04-15 14:21:37 +0300

    week13

--- app.js
+++ app.js
@@ -22,3 +22,40 @@ targetImage.addEventListener('click', () => {
 //shift + alt + F
 
 });
+
+// JSON-ist andmete laadimine ja HTML-i lisamine
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    // Lisa nimi
+    const nimiElem = document.querySelector('.mark-sazonov');
+    if (nimiElem) {
+      nimiElem.textContent = `${data.eesnimi} ${data.perekonnanimi}`;
+    }
+
+    // Lisa e-mail
+    const emailElem = document.querySelector('.mark-sazonov-tallink-ee');
+    if (emailElem) {
+      emailElem.textContent = data.email;
+    }
+
+    // Lisa keeled
+    const keeledElem = document.querySelector('.group-44'); // või '.keeled2' kui tahad teise kohta
+    if (keeledElem && data.keeled) {
+      data.keeled.forEach(keel => {
+        const div = document.createElement('div');
+        div.textContent = keel;
+        keeledElem.appendChild(div);
+      });
+    }
+
+    // Lisa tehnilised oskused (näide)
+    const oskusedElem = document.querySelector('.group-42');
+    if (oskusedElem && data.oskused) {
+      data.oskused.forEach(oskus => {
+        const div = document.createElement('div');
+        div.textContent = oskus;
+        oskusedElem.appendChild(div);
+      });
+    }
+  });
